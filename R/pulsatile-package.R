a4b17031c4c0619585cc9cda996176072e9f6865
Matt Mulvahill (MtEvans) <matthew.mulvahill@ucdenver.edu>
2016-12-19 11:06:53 -0700

    added GPL3 license and added UTF-8 encoding to DESCRIPTION file.  Added a brief description of package to package help file.

--- R/pulsatile-package.R
+++ R/pulsatile-package.R
@@ -1,6 +1,9 @@
-#' Pulsatile package documentation
-#' … bla bla …
+#' @title Pulsatile package documentation
 #' @name pulsatile
+#' @description Bayesian deconvolution model for the analysis of time-series of
+#' pulsatile hormone data. See the help files for the primary functions
+#' pulse_spec() and fit_pulse().
+#'
 #' @docType package
-#' @useDynLib pulsatile
+#' @aliases pulsatile pulsatile-package 
 NULL
