c9ee70260e2d16ba469d605dae4cd1ed53297afb
Matt Mulvahill (annapurna) <matthew.mulvahill@ucdenver.edu>
2017-05-30 10:21:07 -0600

    added Karen's version of t-dstri draw_fixed_effects

--- src/mcmc.c
+++ src/mcmc.c
@@ -912,6 +912,210 @@ void draw_fixed_effects(Node_type *list,
 }
 
 
+/* Karen's version 
+ *
+ * Unknown objects:
+ *   pmean_vch -- prior mean? of ?
+ *   MDBNOR -- ???
+ *
+ * */
+void draw_fixed_effects(Node_type *list, 
+                        Priors *priors, 
+                        Common_parms *parms,
+                        int N, 
+                        unsigned long *seed, 
+                        int iter, 
+                        double **pmean_vch) {
+  // declare variables 
+  Node_type *node;
+  int i, j, k, npulse;
+  double diff_mean_old[2], 
+         diff_mean_new[2],
+         log_prior_ratio,
+         temp_old_mean[2],
+         temp,
+         alpha,
+         *new_mean,
+         *tmpold,
+         *tmpnew,
+         A_inv[2][2],
+         **re_var_inv,
+         **re_var,
+         stdnew[2],
+         stdold[2],
+         log_RE_ratio,
+         new_sum,
+         old_sum,
+         **pvartmp,
+         newint,
+         oldint,
+         junk,
+         iden[2][2],
+         stdxold,
+         stdyold,
+         stdxnew,
+         stdynew,
+         corrxy,
+         log_prop_ratio;
+
+
+  // declare functions 
+  double MDBNOR(double ,double ,double);
+  int rmvnorm(double *, double **, int, double *, unsigned long *,int);
+  double rgamma(double, double, unsigned long *);
+
+  // allocate memory
+  tmpold     = (double *)calloc(2, sizeof(double));
+  tmpnew     = (double *)calloc(2, sizeof(double));
+  new_mean   = (double *)calloc(2, sizeof(double));
+  re_var_inv = (double **)calloc(2, sizeof(double *));
+  re_var     = (double **)calloc(2, sizeof(double *));
+  pvartmp    = (double **)calloc(2, sizeof(double *));
+
+  for (i = 0; i < 2; i++) {
+    re_var_inv[i] = (double *)calloc(2,sizeof(double));
+    re_var[i]     = (double *)calloc(2,sizeof(double));
+    pvartmp[i]    = (double *)calloc(2,sizeof(double));
+  }
+
+  // printf("iter %d \n",iter);
+  nfe++;
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 2; j++) {
+      re_var_inv[i][j] = parms->re_precision[i][j];
+    }
+  }
+
+  if (!cholesky_decomp(re_var_inv,2)) {
+    printf("mass_var_inv in mean mass draw process is not PSD matrix\n");
+    exit(0);
+  }
+
+  re_var = cholesky_invert(2,re_var_inv);
+
+
+  // do a draw for each pulse pair and make the decision for each pair separately 
+  // printf("i %d parms->theta %lf %lf\n",i,parms->theta[0],parms->theta[1]);
+  // for(i=0;i<2;i++)
+  //   for(j=0;j<2;j++)
+  //     printf("i %d j %d pmean_var %lf\n",i,j,pmean_var[i][j]);
+
+  rmvnorm(new_mean, pmean_vch, 2, parms->theta, seed, 1);
+  // printf("new_mean FE %lf %lf\n",new_mean[0],new_mean[1]);
+
+  if (new_mean[0] > 0 && new_mean[1] > 0) {
+    // printf("inside loop\n");
+    diff_mean_old[0] = parms->theta[0] - priors->fe_mean[0];
+    diff_mean_old[1] = parms->theta[1] - priors->fe_mean[1];
+    diff_mean_new[0] = new_mean[0] - priors->fe_mean[0];
+    diff_mean_new[1] = new_mean[1] - priors->fe_mean[1];
+
+
+    // *proposal ratio---ratio of integrals for truncated distribution*
+    pvartmp[0][0] = pmean_vch[0][0] * pmean_vch[0][0];
+    pvartmp[1][1] = pmean_vch[1][1] * pmean_vch[1][1] + pmean_vch[1][0] * pmean_vch[1][0];
+    pvartmp[1][0] = pvartmp[0][1] = pmean_vch[0][0] * pmean_vch[1][0];
+    stdxnew       = (new_mean[0]) / (sqrt(pvartmp[0][0]));
+    stdynew       = (new_mean[1]) / (sqrt(pvartmp[1][1]));
+    stdxold       = (parms->theta[0]) / (sqrt(pvartmp[0][0]));
+    stdyold       = (parms->theta[1]) / (sqrt(pvartmp[1][1]));
+    corrxy        = pvartmp[1][0] / sqrt(pvartmp[0][0] * pvartmp[1][1]);
+
+    // printf("stdxnew %lf stdynew %lf stdxold %lf stdyold %lf\n",stdxnew,stdynew,stdxold,stdyold);
+    newint = MDBNOR(stdxnew,stdynew,corrxy);
+    oldint = MDBNOR(stdxold,stdyold,corrxy);
+    //  printf("newint %lf oldint %lf\n",newint,oldint);
+    log_prop_ratio =  log(newint) - log(oldint);          
+
+    // calculate the ratio of the priors for the decision ratio in the M-H
+    // the log of the true ratio is just the difference of the exponent terms
+    // in the two normal prior distributions of the new and old draws
+    // The denominator adjustment for the truncated normal cancels in this part
+    log_prior_ratio = 0;
+    for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++) {
+        log_prior_ratio += diff_mean_old[i] * priors->fe_precision[i][j] *
+          diff_mean_old[j] - diff_mean_new[i] * priors->fe_precision[i][j] *
+          diff_mean_new[j];
+      }
+    }
+
+    log_prior_ratio *= 0.5;
+    // printf("log_prior_ratio FE %lf \n",log_prior_ratio);
+
+    // calculate the ratio of the distribution of pulse masses and
+    // variances(the random effects)
+    log_RE_ratio = 0;
+
+    node = list->succ;
+    while (node != NULL) {
+
+      // calculate the standardized normal values for the calculation of the
+      // multivariate normal integral
+      stdxnew = (new_mean[0])/(sqrt(re_var[0][0]/node->eta));
+      stdynew = (new_mean[1])/(sqrt(re_var[1][1]/node->eta));
+      stdxold = (parms->theta[0])/(sqrt(re_var[0][0]/node->eta));
+      stdyold = (parms->theta[1])/(sqrt(re_var[1][1]/node->eta));
+      corrxy = re_var[1][0]/sqrt(re_var[0][0] * re_var[1][1]);
+
+      new_sum = 0;
+      old_sum = 0; 
+      tmpold[0] = node->theta[0] - parms->theta[0];
+      tmpold[1] = node->theta[1] - parms->theta[1];
+      tmpnew[0] = node->theta[0] - new_mean[0];
+      tmpnew[1] = node->theta[1] - new_mean[1];
+      // printf("tmpold %lf %lf\n",tmpold[0],tmpold[1]);
+      // printf("tmpnew %lf %lf\n",tmpnew[0],tmpnew[1]);
+
+      temp = 0;
+      for (i=0;i<2;i++) {
+        for (j=0;j<2;j++) {
+          temp += (tmpold[i] * parms->re_precision[i][j] * tmpold[j] - tmpnew[i] * parms->re_precision[i][j] * tmpnew[j]);
+          // printf("temp %lf\n",temp);
+        }
+      }
+
+      temp *= 0.5 * node->eta;
+      newint = MDBNOR(stdxnew,stdynew,corrxy);
+      oldint = MDBNOR(stdxold,stdyold,corrxy);
+      //   printf("newint %lf oldint %lf\n",newint,oldint);
+      temp += log(oldint) - log(newint) ;
+      //     printf("temp end %lf\n",temp);
+      log_RE_ratio += temp;
+      //   printf("log_RE_ratio end %lf \n",log_RE_ratio);
+      node = node->succ;
+    }    
+
+    // printf("log_prior_ratio %lf log_RE_ratio FE %lf log_prop_ratio %lf\n",log_prior_ratio,log_RE_ratio,log_prop_ratio);
+    // printf("log_RE_ratio FE %lf\n",log_RE_ratio);   
+    // printf("log_prop_ratio %lf\n",log_prop_ratio);
+
+    // create the decision on whether to keep the new values or the old value
+    alpha = (0 < (temp =  (log_prior_ratio + log_RE_ratio + log_prop_ratio))) ? 0:temp;
+    // printf("alpha %lf\n",alpha);
+
+    if(log(kiss(seed)) < alpha) {
+      // printf("ACCEPT FE\n");
+      afe++;
+      // printf("acc_mean %d\n",acc_mean);
+      for (i=0;i<2;i++)
+        parms->theta[i] = new_mean[i];
+    }
+  }
+
+  for(i=0;i<2;i++) { 
+    free(re_var_inv[i]);
+    free(re_var[i]);
+  } 
+
+  free(re_var_inv);
+  free(re_var);
+  free(new_mean);
+  free(tmpnew);
+  free(tmpold);
+
+}
+
 
 
 //-----------------------------------------------------------------------------
